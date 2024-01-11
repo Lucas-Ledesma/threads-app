adfb25e08f89659377e85ae54df2bebc572f64ab
LcuasLedesma <lucasfidel99@gmail.com>
2024-01-11 11:11:33 -0300

    svg moved to a component

--- src/app/components/comment/comment.component.ts
+++ src/app/components/comment/comment.component.ts
@@ -1,17 +1,27 @@
 import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { ExpandComponent } from '../../icons/expand/expand.component';
 
 @Component({
   selector: 'app-comment',
   standalone: true,
-  imports: [NgIf],
+  imports: [NgIf, ExpandComponent],
   templateUrl: './comment.component.html',
   styleUrl: './comment.component.scss',
 })
 export class CommentComponent {
   isExpanded = false;
 
+  isReplying = false;
+
   toggelExpanded() {
     this.isExpanded = !this.isExpanded;
+  }
+
+  toggelReplaying() {
+    this.isReplying = !this.isReplying;
+    if (this.isReplying) {
+      this.isExpanded = true;
+    }
   }
 }
---
+++ src/app/icons/expand/expand.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-expand',
+  standalone: true,
+  imports: [],
+  templateUrl: './expand.component.html',
+  styleUrl: './expand.component.scss'
+})
+export class ExpandComponent {
+
+}
